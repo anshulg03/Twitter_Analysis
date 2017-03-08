95584141b5d713e2d5080a4db153da87cbdfccc2
anshulg03 <anshulgupta301@gmail.com>
2017-03-08 19:30:16 +0530

    Feature Modification

--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(shiny)
 # install.packages('rsconnect')
 library(twitteR)
 library(tm)
-library(SnowballC) 
+library(SnowballC)
 library(stringr)
 library(plyr)
 library(rsconnect)
@@ -13,14 +13,15 @@ library(plotly)
 
 # Secret Keys Changed
 consumer_key <- "95zTIl1ucMJK3Otm0Az1zQAw3"
-consumer_secret <- "rQ2uHq5X8bj9c0dIvwbzmxUZnq9gMkoemx5m4bAn2How3vnJqM"
-access_token <- "791290675-ARVTUes6D89Qr5DLQ8tEJGvElfhjZHXXCOaBh2wm"
-access_secret <- "TkFl5fy7wWBBB0Gk1De9n2LJdnKjmA18au4kV8I83U573"
+consumer_secret <-
+  "rQ2uHq5X8bj9c0dIvwbzmxUZnq9gMkoemx5m4bAn2How3vnJqM"
+access_token <- "791290675-ARVTUes6X89Qr5DLQ8tEJGvElfhjZHXXCOaBh2wm"
+access_secret <- "TkFl5fy7wWBBB0Gk1Ge9n2LJdnKjmA18au4kV8I83U573"
 
-setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
-# Define server logic required to draw a histogram
-Commonwords <- function(){
-  wordlist <-file('Stop_words.txt',open='r')
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+
+CommonWords <- function() {
+  wordlist <- file('Stop_words.txt', open = 'r')
   commonwords <- readLines(wordlist)
   close(wordlist)
   commonwords
@@ -28,143 +29,184 @@ Commonwords <- function(){
 
 
 shinyServer(function(input, output, session) {
-   
   TweetDF <- function(searchItem, MaxTerms)
   {
-    SampleTweets <- searchTwitter(searchItem, lang="en", n=MaxTerms, resultType="recent")
-    tweets <-  strip_retweets(SampleTweets, strip_manual = TRUE, strip_mt = TRUE)
+    SampleTweets <-
+      searchTwitter(searchItem,
+                    lang = "en",
+                    n = MaxTerms,
+                    resultType = "recent")
+    tweets <-
+      strip_retweets(SampleTweets,
+                     strip_manual = TRUE,
+                     strip_mt = TRUE)
     df <- twListToDF(tweets)
-    df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub=""))
+    df$text <-
+      sapply(df$text, function(row)
+        iconv(row, "latin1", "ASCII", sub = ""))
     wordcloudentity(df$text)
     return(df)
   }
   
-   cleanTweets <- function(tweets)
+  # Cleaning Tweets, removing links, numbers and symbols.
+  cleanTweets <- function(tweets)
   {
-    tweets <- gsub("http[[:alnum:]]*", "", tweets)
+    tweets <- gsub("https://t.co/[a-z,A-Z,0-9]*{10}","", tweets)
     tweets <- gsub("[[:punct:]]", "", tweets)
     tweets <- gsub("[[:digit:]]", "", tweets)
-    tweets <- str_replace_all(tweets,"#","")
-    tweets <- str_replace_all(tweets,"@","")
+    tweets <- str_replace_all(tweets, "#", "")
+    tweets <- str_replace_all(tweets, "@", "")
     return(tweets)
   }
   
-  wordcloudentity<-function(tweetText)
+  wordcloudentity <- function(tweetText)
   {
-    tweetCorpus<-Corpus(VectorSource(cleanTweets(tweetText)))
-    tweetTDM<-TermDocumentMatrix(tweetCorpus,control=list(removePunctuation=TRUE,
-                                                          stopwords=Commonwords(),
-                                                          removeNumbers=TRUE,tolower=TRUE))
+    tweetCorpus <- Corpus(VectorSource(cleanTweets(tweetText)))
+    tweetTDM <- TermDocumentMatrix(tweetCorpus,
+                              control = list(
+                                              removePunctuation = TRUE,
+                                              stopwords = Commonwords(),
+                                              removeNumbers = TRUE,
+                                              tolower = TRUE
+                                              )
+      ) 
     tdMatrix <- as.matrix(tweetTDM) # creating a data matrix
-    sortedMatrix<-sort(rowSums(tdMatrix),decreasing=TRUE) # calculate row sum of each term and sort in descending order (high freq to low)
-    cloudFrame<-data.frame(word=names(sortedMatrix),freq=sortedMatrix)#extracting names from named list in prev command and binding together into a dataframe with frequencies - called cloudFrame, names in separate columns
-    wcloudentity<-wordcloud(cloudFrame$word,cloudFrame$freq,min.freq = 5,max.words=100, colors=brewer.pal(8,"Dark2"),scale=c(8,1), random.color =F, random.order = F)
+    sortedMatrix <-  sort(rowSums(tdMatrix), decreasing = TRUE) # calculate row sum of each term and sort in descending order (high freq to low)
+    cloudFrame <-   data.frame(word = names(sortedMatrix), freq = sortedMatrix)#extracting names from named list in prev command and binding together into a dataframe with frequencies - called cloudFrame, names in separate columns
+    wcloudentity <- wordcloud(
+                              cloudFrame$word,
+                              cloudFrame$freq,
+                              min.freq = 5,
+                              max.words = 100,
+                              colors = brewer.pal(8, "Dark2"),
+                              scale = c(7, 0.8),
+                              random.color = F,
+                              random.order = F
+                              )
   }
   
   score.sentiment = function(sentences, pos.words, neg.words)
   {
-    
     scores = laply(sentences, function(sentence, pos.words, neg.words) {
-      
       # clean up sentences with R's regex-driven global substitute, gsub():
       sentence = cleanTweets(sentence)
-      # and convert to lower case:
+      
       sentence = tolower(sentence)
       # split into words. str_split is in the stringr package
       word.list = str_split(sentence, '\\s+')
-      # sometimes a list() is one level of hierarchy too much
+      
       words = unlist(word.list)
       
-      
-      # compare our words to the dictionaries of positive & negative terms
+      # compare our words to the dictionaries
       pos.matches = match(words, pos.words)
       neg.matches = match(words, neg.words)
       
       # match() returns the position of the matched term or NA
-      # we just want a TRUE/FALSE:
       pos.matches = !is.na(pos.matches)
       neg.matches = !is.na(neg.matches)
       
-      # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
       score = sum(pos.matches) - sum(neg.matches)
       
       return(score)
     }, pos.words, neg.words)
-    scores.df = data.frame(score=scores, text=sentences, size=seq(length(scores)))
+    scores.df = data.frame(score = scores,
+                           text = sentences,
+                           size = seq(length(scores)))
     return(scores.df)
   }
   
-  sentimentalanalysis<-function(entity,nooftweets){
-    
-    positivewords=readLines("positive-words.txt")
-    negativewords=readLines("negative-words.txt")
-    entityscore = score.sentiment(cleanTweets(entity$text),positivewords,negativewords)
-    entityscore$posi = sum(laply(entityscore$score,function(score){
-      if(score>0){
+  sentimentalanalysis <- function(entity, nooftweets) {
+    positivewords = readLines("positive-words.txt")
+    negativewords = readLines("negative-words.txt")
+    entityscore = score.sentiment(cleanTweets(entity$text), positivewords, negativewords)
+    entityscore$posi = sum(laply(entityscore$score, function(score) {
+      if (score > 0) {
         return(1)
       }
-      return(0)}))
-    entityscore$negi = sum(laply(entityscore$score,function(score){
-      if(score<0){
+      return(0)
+    }))
+    entityscore$negi = sum(laply(entityscore$score, function(score) {
+      if (score < 0) {
         return(1)
       }
-      return(0)}))
-    entityscore$neu = nooftweets-(entityscore$posi+entityscore$negi)
+      return(0)
+    }))
+    entityscore$neu = nooftweets - (entityscore$posi + entityscore$negi)
     
     entityscore
-  }   
+  }
   
-  entity1score <-function(){
-    entity1<-entityone()
-    entity1<-subset(entity1,!is.na(entity1$text))
-    scores <-sentimentalanalysis(entity1,nrow(entity1))
+  entity1score <- function() {
+    entity1 <- entityone()
+    entity1 <- subset(entity1, !is.na(entity1$text))
+    scores <- sentimentalanalysis(entity1, nrow(entity1))
     scores
   }
   
-  
-  
-  output$entity1wc<-renderText({
-    
-    input$entity1})
-  
-  output$entity1wcplot<-renderPlot({
-    click()})
-  
-  entityone<- eventReactive(input$go,{
-    entityone<-TweetDF(input$entity1, input$maxTweets)
-  })
 
-  click <- eventReactive(input$go,{
-       
-    withProgress(message = 'Generating data', detail = "part 0", value = 0, {
-        incProgress(0.5, detail = paste(50, " %"))
-      wordcloudentity(entityone()$text)
-
-    })
-    
+  output$entity1wc <- renderText({
+    input$entity1
   })
   
-
-    
-    output$plot3<- renderPlotly({
-      withProgress(message = 'Analysing Tweets', detail = "part 0", value = 0, {
-        incProgress(0.5, detail = paste(50, " %"))
-      
-      a<-entity1score()
-      lbl <- c('Positive','Negative','Neutral')
-      cnt <- c(a$posi[1],a$negi[1],a$neu[1])
-      data <- data.frame(lbl,cnt)
-      p <- plot_ly(data, labels = ~lbl, values = ~cnt, type = 'pie',textposition = 'inside',
-                   textinfo = 'label+percent',
-                   insidetextfont = list(color = '#FFFFFF'),
-                   hoverinfo = ~lbl) %>%
-               layout(title = 'Tweet Sentiment',
-               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-                
-      return(p)
-      })
-      })
-    
+  output$entity1wcplot <- renderPlot({
+    click()
   })
   
+  entityone <- eventReactive(input$go, {
+    entityone <- TweetDF(input$entity1, input$maxTweets)
+  })
+  
+  click <- eventReactive(input$go, {
+    withProgress(message = 'Getting Tweets',
+                 
+                 {
+                   incProgress(0.5, detail = "50 %")
+                   wordcloudentity(entityone()$text)
+                 })
+  })
+  
+  
+  
+  output$plot3 <- renderPlotly({
+    withProgress(message = 'Analysing Tweets',
+                 detail = "part 0",
+                 value = 0,
+                 {
+                   incProgress(0.5, detail = paste(50, " %"))
+                   
+                   a <- entity1score()
+                   lbl <- c('Positive', 'Negative', 'Neutral')
+                   cnt <- c(a$posi[1], a$negi[1], a$neu[1])
+                   data <- data.frame(lbl, cnt)
+                   p <- plot_ly(
+                               data,
+                               labels = ~ lbl,
+                               values = ~ cnt,
+                               type = 'pie',
+                               textposition = 'inside',
+                               textinfo = 'label+percent',
+                               insidetextfont = list(color = '#FFFFFF'),
+                               hoverinfo = ~ lbl
+                                ) %>%
+                        layout(
+                               title = 'Tweet Sentiment',
+                               xaxis = list(
+                                             showgrid = FALSE,
+                                             zeroline = FALSE,
+                                             showticklabels = FALSE
+                                            ),
+                               yaxis = list(
+                                             showgrid = FALSE,
+                                             zeroline = FALSE,
+                                             showticklabels = FALSE
+                                            )
+                              )
+                   return(p)
+                 })
+  })
+  
+  output$dframe <- renderTable(
+    cleanTweets(entityone()$text)
+  )
+  
+})
--- ui.R
+++ ui.R
@@ -4,37 +4,67 @@ library(plotly)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
   # Application title
   dashboardPage(
-    dashboardHeader(title="Twitter Sentiments"),
+    dashboardHeader(title = "Twitter Sentiments"),
     dashboardSidebar(
-      textInput("entity1", "Handle: ",""),
-      sliderInput("maxTweets","Number of recent tweets to use for analysis:",min=300,max=2500,value=600,step=100),
-      actionButton(inputId='go',icon =icon("twitter"), label="Hit it!"),
+      textInput("entity1", "Handle: ", "",placeholder = "Enter Handle or Topic"),
+      sliderInput(
+        "maxTweets",
+        "Number of recent tweets to use for analysis:",
+        min = 300,
+        max = 2500,
+        value = 600,
+        step = 100
+      ),
+      actionButton(
+        inputId = 'go',
+        icon = icon("twitter"),
+        label = "Hit it!",
+        style="color: #fff; background-color: #337ab7; border-color: #2e6da4;",
+        HTML("100%")
+        
+      ),
       hr(),
       sidebarMenu(
-        # menuItem("Sentiment Analysis", tabName = "dashboard", icon = icon("signal")),
-        menuItem("Word Clouds", tabName = "wordcloud", icon = icon("cloud")),
-        menuItem("Score", tabName = "score", icon = icon("dashboard"))
+        menuItem("Word Cloud", tabName = "wordcloud", icon = icon("cloud")),
+        menuItem("Score", tabName = "score", icon = icon("dashboard")),
+        menuItem("Tweets", tabName = "tweet", icon = icon("cloud")),
+        HTML("<br>"),
+        helpText(
+          "Note: It may take some time to load",
+          HTML("<br>"),
+          "Patience is a Virtue, my Friend"
+        )
       )
     ),
     
     # Show a plot of the generated distribution
-    dashboardBody(
-      tabItems(
-
-        tabItem(tabName = "wordcloud",
-                HTML("<div style='color:#fff; font-size: 25px;text-align: center; background-color:#1E282C; box-shadow: 0 12px 6px -6px #777;border-radius: 10px;padding: 9px 15px ; margin-bottom:8px;' >WordCloud</div>"),
-                plotOutput("entity1wcplot")
+    dashboardBody(tabItems(
+      tabItem(
+        tabName = "wordcloud",
+        HTML(
+          "<div style='color:#fff; font-size: 25px;text-align: center; background-color:#1E282C; box-shadow: 0 12px 6px -6px #777;border-radius: 10px;padding: 9px 15px ; margin-bottom:8px;' >WordCloud</div>"
         ),
-        tabItem(tabName = "score",
-                HTML("<div style='color:#fff; font-size: 25px;text-align: center; background-color:#1E282C; box-shadow: 0 12px 6px -6px #777;border-radius: 10px;padding: 9px 15px ; margin-bottom:8px;' >Score</div>"),
-                HTML("<br>"),
-                plotlyOutput("plot3")
-
-        )
-        )
+        plotOutput("entity1wcplot")
+      ),
+      tabItem(
+        tabName = "score",
+        HTML(
+          "<div style='color:#fff; font-size: 25px;text-align: center; background-color:#1E282C; box-shadow: 0 12px 6px -6px #777;border-radius: 10px;padding: 9px 15px ; margin-bottom:8px;' >Score</div>"
+        ),
+        HTML("<br>"),
+        plotlyOutput("plot3")
+      ),
+      tabItem(
+        tabName = "tweet",
+        HTML(
+          "<div style='color:#fff; font-size: 25px;text-align: center; background-color:#1E282C; box-shadow: 0 12px 6px -6px #777;border-radius: 10px;padding: 9px 15px ; margin-bottom:8px;' >Tweets</div>"
+        ),
+        HTML("<br>"),
+        tableOutput("dframe")
       )
-    )
-  ))
+    ))
+  )
+))
+
